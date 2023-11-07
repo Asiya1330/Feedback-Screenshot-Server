8bb9f6cedeed03b91e5ee18f578fd9104e044f6a
Asiya <asiya.batool987@gmail.com>
2023-11-08 03:30:50 +0500

    --amend

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const Sequelize = require("sequelize");
-
 const sequelize = new Sequelize({
   dialect: process.env.DIALECT,
   host: process.env.HOST,
-  username: process.env.USERNAME,
+  username: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.PORT,
